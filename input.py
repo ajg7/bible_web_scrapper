b59fb2f5e0e7483651aec2b1735aa83018d071f6
AJ Gebara <49355730+ajg7@users.noreply.github.com>
2021-03-04 12:29:53 -0600

    defined the input class

---
+++ input.py
@@ -0,0 +1,4 @@
+class Input:
+    def __init__(self, book, verse):
+        self.book = book;
+        self.verse = verse;
---
+++ scrapper.py
@@ -0,0 +1,8 @@
+# The Plan:
+
+# Inspect Bible Hub Text Analysis
+# Scrape Bible Hub
+# Parse the Data
+# Create A window, in which you can input a verse, and it then scrapes and parses the data from Bible Hub
+# Display it in a similar format
+
