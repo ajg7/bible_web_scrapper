8e6c6ea7c1d344826900205e5fc3a67b9fb3610c
AJ Gebara <49355730+ajg7@users.noreply.github.com>
2021-03-04 13:57:15 -0600

    hebrew text is now in a list called text

--- scrapper.py
+++ scrapper.py
@@ -1,8 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
 # The Plan:
 
 # Inspect Bible Hub Text Analysis
 # Scrape Bible Hub
+    # We want the Hebrew Text, English Text, and Morphology
 # Parse the Data
 # Create A window, in which you can input a verse, and it then scrapes and parses the data from Bible Hub
 # Display it in a similar format
 
+URL = "https://biblehub.com/interlinear/songs/1-3.htm"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+# Grab the div (class = maintext)
+
+results = soup.find(class_="padleft")
+# print(results.prettify())
+
+text = []
+
+for result in results:
+    hebrewText = result.find("span", class_="hebrew")
+    if hebrewText == None:
+        continue
+    else:
+        text.append(hebrewText.text)
+    print(hebrewText.text, "Shalom")
+
+print(text, "Hi")
